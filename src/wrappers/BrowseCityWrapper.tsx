0f2470a7581f9a1d0387623c649fc5f9a8df0604
mardhamardiya.18 <mardhamardiya.18@gmail.com>
2024-10-07 22:46:37 +0800

    Integrate cities data with API

--- src/wrappers/BrowseCityWrapper.tsx
+++ src/wrappers/BrowseCityWrapper.tsx
@@ -1,7 +1,39 @@
 import { Swiper, SwiperSlide } from "swiper/react";
 import { CityCard } from "../components/CityCard";
+import { useEffect, useState } from "react";
+import { City } from "../types/type";
+import axios from "axios";
 
 export const BrowseCityWrapper = () => {
+  const [cities, setCities] = useState<City[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    axios
+      .get("http://127.0.0.1:8000/api/cities", {
+        headers: {
+          "X-API-KEY": "9188hjhsakhs8",
+        },
+      })
+      .then((response) => {
+        setCities(response.data.data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        setError(error);
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
+  if (error) {
+    return <p>Error loading data: {error}</p>;
+  }
+
   return (
     <>
       <section id="Cities" className="flex flex-col gap-[30px] mt-[100px]">
@@ -25,9 +57,14 @@ export const BrowseCityWrapper = () => {
             slidesOffsetAfter={30}
             slidesOffsetBefore={30}
           >
-            <SwiperSlide className="!w-fit first-of-type:pl-[calc((100%-1130px-60px)/2)] last-of-type:pr-[calc((100%-1130px-60px)/2)]">
-              <CityCard />
-            </SwiperSlide>
+            {cities.map((city) => (
+              <SwiperSlide
+                key={city.id}
+                className="!w-fit first-of-type:pl-[calc((100%-1130px-60px)/2)] last-of-type:pr-[calc((100%-1130px-60px)/2)]"
+              >
+                <CityCard city={city} />
+              </SwiperSlide>
+            ))}
           </Swiper>
         </div>
       </section>
